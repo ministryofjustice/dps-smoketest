c97baa216d8222d73c211c356a3b6a760a21c715
Mike Halma <mike.halma@digital.justice.gov.uk>
2020-11-11 10:49:34 +0000

    DT-1274 Use latest versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ dependencies {
   implementation("org.springdoc:springdoc-openapi-kotlin:1.4.8")
 
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
-  testImplementation("io.swagger.parser.v3:swagger-parser-v2-converter:2.0.21")
+  testImplementation("io.swagger.parser.v3:swagger-parser-v2-converter:2.0.23")
   testImplementation("io.projectreactor:reactor-test:3.4.0")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
 }
