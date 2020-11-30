16b562983681ccaaad82df32dcb50460cd015efa
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2020-11-30 11:46:32 +0000

    DT-1260 Fix timing issue with ptpu smoke test (#17)
    * DT-1260 Fix timing issue with ptpu smoke test
    
    * DT-1260 Fix timing issue with ptpu smoke test
    
    * DT-1260 Remove debug logging

--- src/main/kotlin/uk/gov/justice/digital/hmpps/dpssmoketest/service/SmokeTestService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/dpssmoketest/service/SmokeTestService.kt
@@ -1,11 +1,15 @@
 package uk.gov.justice.digital.hmpps.dpssmoketest.service
 
+import org.reactivestreams.Publisher
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Flux
-import uk.gov.justice.digital.hmpps.dpssmoketest.resource.SmokeTestResource
+import reactor.core.publisher.Mono
+import uk.gov.justice.digital.hmpps.dpssmoketest.resource.SmokeTestResource.TestStatus
+import uk.gov.justice.digital.hmpps.dpssmoketest.resource.SmokeTestResource.TestStatus.TestProgress.COMPLETE
 import uk.gov.justice.digital.hmpps.dpssmoketest.service.ptpu.CommunityService
+import java.time.Duration
 
-data class PtpuTestInputs(val crn: String, val nomsNumber: String, val bookingNumber: String, val prisonCode: String, val testStatus: SmokeTestResource.TestStatus)
+data class PtpuTestInputs(val crn: String, val nomsNumber: String, val bookingNumber: String, val prisonCode: String, val testStatus: TestStatus)
 
 @Service
 class SmokeTestService(
@@ -13,7 +17,7 @@ class SmokeTestService(
   private val communityService: CommunityService,
 ) {
 
-  fun runSmokeTest(testProfile: PtpuTestParameters): Flux<SmokeTestResource.TestStatus> {
+  fun runSmokeTest(testProfile: PtpuTestParameters): Flux<TestStatus> {
     return Flux.from(prisonService.getTestInputs(testProfile.nomsNumber, testProfile.crn))
       .flatMap {
         Flux.concat(
@@ -21,10 +25,16 @@ class SmokeTestService(
           Flux.from(communityService.resetTestData(it.crn)),
           Flux.from(prisonService.triggerTest(it.nomsNumber)),
           communityService.waitForTestToComplete(it.nomsNumber, it.bookingNumber),
+          waitForUpdates(),
           Flux.from(
             communityService.assertTestResult(it.nomsNumber, it.bookingNumber, it.prisonCode)
           )
-        ).takeUntil(SmokeTestResource.TestStatus::hasResult)
+        ).takeUntil(TestStatus::hasResult)
       }
   }
+
+  private fun waitForUpdates(): Publisher<out TestStatus> =
+    Flux.interval(Duration.ofMillis(1000))
+      .take(1)
+      .flatMap { Mono.just(TestStatus("Waiting for final update", COMPLETE)) }
 }
