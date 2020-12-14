ec54dd74cb758aa35d0dc29635786590f8a989df
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2020-12-14 16:23:48 +0000

    DT-1398 Upgrade gradle plugin (#18)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "2.0.0"
-  kotlin("plugin.spring") version "1.4.10"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "2.1.0"
+  kotlin("plugin.spring") version "1.4.21"
 }
 
 configurations {
@@ -13,12 +13,12 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 
-  implementation("org.springdoc:springdoc-openapi-ui:1.5.0")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.5.0")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.0")
+  implementation("org.springdoc:springdoc-openapi-ui:1.5.1")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.5.1")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.1")
 
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
-  testImplementation("io.swagger.parser.v3:swagger-parser-v2-converter:2.0.23")
-  testImplementation("io.projectreactor:reactor-test:3.4.0")
+  testImplementation("io.swagger.parser.v3:swagger-parser-v2-converter:2.0.24")
+  testImplementation("io.projectreactor:reactor-test:3.4.1")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
 }
