494a84eb8aee23e2cbba6c626f26469a2420554f
Andy Marke <andy.marke@digital.justice.gov.uk>
2021-02-08 11:54:46 +0000

    DT-1595 More plumbing for community API calls

--- src/main/kotlin/uk/gov/justice/digital/hmpps/dpssmoketest/service/PsiSmokeTestService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/dpssmoketest/service/PsiSmokeTestService.kt
@@ -4,8 +4,7 @@ import org.springframework.stereotype.Service
 import reactor.core.publisher.Flux
 import uk.gov.justice.digital.hmpps.dpssmoketest.resource.SmokeTestResource.TestStatus
 import uk.gov.justice.digital.hmpps.dpssmoketest.resource.SmokeTestResource.TestStatus.TestProgress.COMPLETE
-
-data class PsiTestInputs(val crn: String, val testStatus: TestStatus)
+import kotlin.random.Random.Default.nextInt
 
 @Service
 class PsiSmokeTestService(
@@ -14,6 +13,26 @@ class PsiSmokeTestService(
 ) {
 
   fun runSmokeTest(testProfile: PsiTestParameters): Flux<TestStatus> {
-    return Flux.just(TestStatus("All done", COMPLETE))
+    val (firstName, surname) = generateRandomNames()
+    return Flux.concat(
+      Flux.from(communityService.checkOffenderExists(testProfile.crn)),
+      Flux.just(TestStatus("Will update name to $firstName $surname")),
+      Flux.from(communityService.setOffenderDetailsTestData(testProfile.crn, firstName, surname)),
+      Flux.just(TestStatus("All done", COMPLETE))
+    ).takeUntil(TestStatus::hasResult)
   }
+}
+
+private fun generateRandomNames(): Pair<String, String> {
+  return randomString() to randomString()
+}
+
+
+private fun randomString(): String {
+  val charPool: List<Char> = ('a'..'z') + ('A'..'Z')
+
+  return (1..10)
+    .map { nextInt(0, charPool.size) }
+    .map(charPool::get)
+    .joinToString("")
 }
--- src/test/kotlin/uk/gov/justice/digital/hmpps/dpssmoketest/integration/PsiSmokeTestIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/dpssmoketest/integration/PsiSmokeTestIntegrationTest.kt
@@ -1,14 +1,23 @@
 package uk.gov.justice.digital.hmpps.dpssmoketest.integration
 
+import com.github.tomakehurst.wiremock.client.WireMock
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
 import org.springframework.http.MediaType.TEXT_EVENT_STREAM
+import org.springframework.test.web.reactive.server.FluxExchangeResult
 import reactor.test.StepVerifier
 import uk.gov.justice.digital.hmpps.dpssmoketest.helper.JwtAuthHelper
+import uk.gov.justice.digital.hmpps.dpssmoketest.integration.wiremock.CommunityApiExtension
 import uk.gov.justice.digital.hmpps.dpssmoketest.resource.SmokeTestResource.TestStatus
 import uk.gov.justice.digital.hmpps.dpssmoketest.resource.SmokeTestResource.TestStatus.TestProgress.FAIL
+import java.net.HttpURLConnection
+import java.net.HttpURLConnection.HTTP_INTERNAL_ERROR
+import java.net.HttpURLConnection.HTTP_NOT_FOUND
 
 class PsiSmokeTestIntegrationTest : IntegrationTestBase() {
 
@@ -62,4 +71,77 @@ class PsiSmokeTestIntegrationTest : IntegrationTestBase() {
         .expectStatus().isOk
     }
   }
+
+  @Nested
+  @DisplayName("When offender initial check fails")
+  inner class WhenOffenderInitialCheckFails {
+    @BeforeEach
+    internal fun setUp() {
+      stubCheckOffenderExists(HTTP_NOT_FOUND)
+    }
+
+    @Test
+    fun `fails after first step`() {
+      val results = postStartTest()
+
+      StepVerifier.create(results.responseBody)
+        .expectNext(TestStatus("Offender we expected to exist X379864 was not found. Check the offender has not be deleted in Delius", FAIL))
+        .verifyComplete()
+    }
+  }
+
+  @Nested
+  @DisplayName("When unable to change name")
+  inner class WhenChangingNameFails {
+    @BeforeEach
+    internal fun setUp() {
+      stubCheckOffenderExists()
+      stubChangeOffenderName(HTTP_INTERNAL_ERROR)
+    }
+
+    @Test
+    fun `fails after third step`() {
+      val results = postStartTest()
+
+      StepVerifier.create(results.responseBody)
+        .expectNextMatches { testResult -> testResult.description.contains("Offender X379864 exists and is good to go") }
+        .expectNextMatches { testResult -> testResult.description.contains("Will update name to") }
+        .expectNextMatches { testResult -> testResult.description.contains("Update offender details test failed for X379864 failed due t") }
+        .verifyComplete()
+    }
+  }
+
+  private fun postStartTest(): FluxExchangeResult<TestStatus> =
+    webTestClient.post()
+      .uri("/smoke-test/probation-search-indexer/PSI_T3")
+      .accept(TEXT_EVENT_STREAM)
+      .headers(jwtAuthHelper.setAuthorisation("dps-smoke-test", listOf("ROLE_SMOKE_TEST")))
+      .exchange()
+      .expectStatus().isOk
+      .returnResult(TestStatus::class.java)
+
 }
+
+private fun stubCheckOffenderExists(status: Int = HttpURLConnection.HTTP_OK) =
+  CommunityApiExtension.communityApi.stubFor(
+    WireMock.get(WireMock.anyUrl()).willReturn(
+      WireMock.aResponse()
+        .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+        .withStatus(status)
+        .withBody(
+          """
+            { "firstName": "Jane", "surname": "Smith" }
+            """.trimIndent()
+        )
+    )
+  )
+
+private fun stubChangeOffenderName(status: Int = HttpURLConnection.HTTP_OK) =
+  CommunityApiExtension.communityApi.stubFor(
+    WireMock.post(WireMock.anyUrl()).willReturn(
+      WireMock.aResponse()
+        .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+        .withStatus(status)
+    )
+  )
+
