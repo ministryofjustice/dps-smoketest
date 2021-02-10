2e3f4df142e8b08fcacd2ee6308f068fad7baec4
Andy Marke <andy.marke@digital.justice.gov.uk>
2021-02-10 09:51:00 +0000

    Dt 1595 probation search indexer (#26)
    * DT-1595 Revert data back to PSI Smoketest
    
    * DT-1595 Revert data back to PSI Smoketest

--- src/test/kotlin/uk/gov/justice/digital/hmpps/dpssmoketest/integration/PsiSmokeTestIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/dpssmoketest/integration/PsiSmokeTestIntegrationTest.kt
@@ -1,7 +1,11 @@
 package uk.gov.justice.digital.hmpps.dpssmoketest.integration
 
 import com.github.tomakehurst.wiremock.client.WireMock
+import com.github.tomakehurst.wiremock.client.WireMock.equalToJson
+import com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor
+import com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo
 import com.github.tomakehurst.wiremock.stubbing.Scenario
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
@@ -216,8 +220,23 @@ class PsiSmokeTestIntegrationTest : IntegrationTestBase() {
             testResult.progress == SUCCESS
         }
         .verifyComplete()
+
+      // and data reverted back to PSI Smoketest
+      CommunityApiExtension.communityApi.verify(
+        postRequestedFor(urlEqualTo("/secure/smoketest/offenders/crn/X379864/details")).withRequestBody(
+          equalToJson(
+            """{
+                "firstName" : "PSI",
+                "surname": "Smoketest"
+            }""".trimMargin()
+          )
+        )
+      )
+      assertThat(getLastRequest().request.bodyAsString).contains("PSI").contains("Smoketest")
     }
   }
+
+  private fun getLastRequest() = CommunityApiExtension.communityApi.serveEvents.requests.first()
 
   private fun postStartTest(): FluxExchangeResult<TestStatus> =
     webTestClient.post()
