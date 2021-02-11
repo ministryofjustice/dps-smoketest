7b032fcf2cc3ca11c132546bfe90b8607827e753
Andy Marke <andy.marke@digital.justice.gov.uk>
2021-02-11 11:32:37 +0000

    DT-1595 AppInsights and plugin upgrade (#28)
    * DT-1595 AppInsights and plugin upgrade
    
    * DT-1595 AppInsights and plugin upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "2.1.2"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.1.0"
   kotlin("plugin.spring") version "1.4.21"
 }
 
@@ -13,12 +13,12 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 
-  implementation("org.springdoc:springdoc-openapi-ui:1.5.1")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.5.1")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.1")
+  implementation("org.springdoc:springdoc-openapi-ui:1.5.4")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.5.4")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.4")
 
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
   testImplementation("io.swagger.parser.v3:swagger-parser-v2-converter:2.0.24")
-  testImplementation("io.projectreactor:reactor-test:3.4.1")
+  testImplementation("io.projectreactor:reactor-test:3.4.2")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
 }
