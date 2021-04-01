0e2ffa5c9ec236edd57b6bf4ac3468511c4c3bb5
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2021-04-01 11:49:25 +0100

    DT-1829 Upgrades (#34)
    * DT-1829 useLatestVersions
    
    * DT-1829 upgrade kotlin spring version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.1.1"
-  kotlin("plugin.spring") version "1.4.30"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.1.4"
+  kotlin("plugin.spring") version "1.4.32"
 }
 
 configurations {
@@ -13,12 +13,12 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 
-  implementation("org.springdoc:springdoc-openapi-ui:1.5.4")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.5.4")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.4")
+  implementation("org.springdoc:springdoc-openapi-ui:1.5.6")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.5.6")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.6")
 
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
   testImplementation("io.swagger.parser.v3:swagger-parser-v2-converter:2.0.24")
-  testImplementation("io.projectreactor:reactor-test:3.4.3")
+  testImplementation("io.projectreactor:reactor-test:3.4.4")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
 }
