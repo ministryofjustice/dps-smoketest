bc020a0f73060b0f4ea1dc16dfedef25f1207b8c
simonmitchell <simon.mitchell@digital.justice.gov.uk>
2021-05-18 15:55:35 +0100

    DT-2051: ⬆️ Upgrade to app insights 3.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.2.0-beta"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.2.1"
   kotlin("plugin.spring") version "1.5.0"
 }
 
@@ -13,13 +13,13 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 
-  implementation("org.springdoc:springdoc-openapi-ui:1.5.6")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.5.6")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.6")
+  implementation("org.springdoc:springdoc-openapi-ui:1.5.8")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.5.8")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.8")
 
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
-  testImplementation("io.swagger.parser.v3:swagger-parser-v2-converter:2.0.24")
-  testImplementation("io.projectreactor:reactor-test:3.4.4")
+  testImplementation("io.swagger.parser.v3:swagger-parser-v2-converter:2.0.25")
+  testImplementation("io.projectreactor:reactor-test:3.4.6")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
 }
 
