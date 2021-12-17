f918892879e39ddcc894efb96075b8b9db917810
simonmitchell <simon.mitchell@digital.justice.gov.uk>
2021-12-17 15:53:02 +0000

    DT-2975: ⬆️ Upgrade hmpps-gradle-springboot plugin to 4.0.0 includes springboot 2.6.1 and kotlin 1.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.16"
-  kotlin("plugin.spring") version "1.5.31"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.0.0"
+  kotlin("plugin.spring") version "1.6.0"
 }
 
 configurations {
@@ -13,18 +13,17 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.0.2")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.0.3")
 
-  implementation("org.springdoc:springdoc-openapi-ui:1.5.10")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.5.10")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.10")
-  implementation("org.awaitility:awaitility-kotlin:4.1.0")
+  implementation("org.springdoc:springdoc-openapi-ui:1.6.1")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.1")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.1")
+  implementation("org.awaitility:awaitility-kotlin:4.1.1")
 
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
-  testImplementation("io.swagger.parser.v3:swagger-parser-v2-converter:2.0.27")
-  testImplementation("io.projectreactor:reactor-test:3.4.9")
+  testImplementation("io.swagger.parser.v3:swagger-parser-v2-converter:2.0.28")
+  testImplementation("io.projectreactor:reactor-test:3.4.13")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
-  testImplementation("org.mockito.kotlin:mockito-kotlin:4.0.0")
 }
 
 java {
