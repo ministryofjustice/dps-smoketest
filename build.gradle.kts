fc14c934b79bac79575ec77bd9ad9262282b0a29
SimonMitchellMOJ <51327941+SimonMitchellMOJ@users.noreply.github.com>
2022-04-27 12:26:24 +0100

    HAAR-364: ⬆️ upgrade to hmpps.gradle-spring-boot 4.1.5 (#82)
    * HAAR-364: ⬆️ upgrade to hmpps.gradle-spring-boot 4.1.5
    
    * HAAR-364: removed un-necessary copy of cert
    
    * HAAR-364: removed un-necessary copy of cert

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.0.0"
-  kotlin("plugin.spring") version "1.6.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.1.5"
+  kotlin("plugin.spring") version "1.6.21"
 }
 
 configurations {
@@ -13,27 +13,27 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.0.3")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.1.3")
 
-  implementation("org.springdoc:springdoc-openapi-ui:1.6.1")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.1")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.1")
-  implementation("org.awaitility:awaitility-kotlin:4.1.1")
+  implementation("org.springdoc:springdoc-openapi-ui:1.6.8")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.8")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.8")
+  implementation("org.awaitility:awaitility-kotlin:4.2.0")
 
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
-  testImplementation("io.swagger.parser.v3:swagger-parser-v2-converter:2.0.28")
-  testImplementation("io.projectreactor:reactor-test:3.4.13")
+  testImplementation("io.swagger.parser.v3:swagger-parser-v2-converter:2.0.32")
+  testImplementation("io.projectreactor:reactor-test:3.4.17")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
 }
 
 java {
-  toolchain.languageVersion.set(JavaLanguageVersion.of(16))
+  toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 }
 
 tasks {
   withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions {
-      jvmTarget = "16"
+      jvmTarget = "17"
     }
   }
 }
