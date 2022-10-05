25e1763e7a56c1e6e8f20a657b4f561c720c05c5
petergphillips <peter@greenthistle.co.uk>
2022-10-05 10:33:51 +0100

    SDI-409: ⬆️  Grab latest spring boot gradle plugin (#87)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.5.1"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.5.4"
   kotlin("plugin.spring") version "1.7.10"
 }
 
@@ -13,7 +13,7 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.1.10")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.1.11")
 
   implementation("org.springdoc:springdoc-openapi-ui:1.6.11")
   implementation("org.springdoc:springdoc-openapi-data-rest:1.6.11")
@@ -21,7 +21,7 @@ dependencies {
   implementation("org.awaitility:awaitility-kotlin:4.2.0")
 
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
-  testImplementation("io.swagger.parser.v3:swagger-parser-v2-converter:2.1.2")
+  testImplementation("io.swagger.parser.v3:swagger-parser-v2-converter:2.1.3")
   testImplementation("io.projectreactor:reactor-test:3.4.23")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
 }
