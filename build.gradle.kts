02719bc760fc80ee541c936cf3fc678653c7ae41
petergphillips <peter@greenthistle.co.uk>
2022-11-02 09:31:36 +0000

    SDI-438: ⬆️  Upgrade to latest for spring security 5.7.5 (#93)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.5.6"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.5.7-beta"
   kotlin("plugin.spring") version "1.7.20"
 }
 
@@ -21,7 +21,7 @@ dependencies {
   implementation("org.awaitility:awaitility-kotlin:4.2.0")
 
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
-  testImplementation("io.swagger.parser.v3:swagger-parser-v2-converter:2.1.6")
+  testImplementation("io.swagger.parser.v3:swagger-parser-v2-converter:2.1.7")
   testImplementation("io.projectreactor:reactor-test:3.4.24")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
 }
