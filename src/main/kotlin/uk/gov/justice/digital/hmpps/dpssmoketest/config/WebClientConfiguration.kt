469a403ca4e285e26abada1b0a540aa7a876a0ea
Peter Phillips <peter@greenthistle.co.uk>
2023-10-30 13:28:32 +0000

    SDIT-1129: 🐛 Add timeouts and remove unnecessary configuration

--- src/main/kotlin/uk/gov/justice/digital/hmpps/dpssmoketest/config/WebClientConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/dpssmoketest/config/WebClientConfiguration.kt
@@ -3,6 +3,7 @@ package uk.gov.justice.digital.hmpps.dpssmoketest.config
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder
@@ -10,71 +11,55 @@ import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction
 import org.springframework.web.reactive.function.client.WebClient
+import reactor.netty.http.client.HttpClient
+import java.time.Duration
+import kotlin.apply as kotlinApply
 
 @Configuration
 class WebClientConfiguration(
   @Value("\${community.endpoint.url}") private val communityRootUri: String,
   @Value("\${prisonapi.endpoint.url}") private val prisonapiRootUri: String,
-  @Value("\${oauth.endpoint.url}") private val oauthRootUri: String,
   @Value("\${probationoffendersearch.endpoint.url}") private val probationOffenderSearchUri: String,
+  @Value("\${api.timeout:10s}") val timeout: Duration,
 ) {
 
   @Bean
-  fun communityApiWebClient(authorizedClientManager: OAuth2AuthorizedClientManager): WebClient {
-    val oauth2Client = ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager)
-    oauth2Client.setDefaultClientRegistrationId("community-api")
+  fun communityApiWebClient(authorizedClientManager: OAuth2AuthorizedClientManager, builder: WebClient.Builder): WebClient =
+    authorisedWebClient(authorizedClientManager, builder, registrationId = "community-api", url = communityRootUri)
 
-    return WebClient.builder()
-      .baseUrl(communityRootUri)
-      .apply(oauth2Client.oauth2Configuration())
+  @Bean
+  fun prisonApiWebClient(authorizedClientManager: OAuth2AuthorizedClientManager, builder: WebClient.Builder): WebClient =
+    authorisedWebClient(authorizedClientManager, builder, registrationId = "prison-api", url = prisonapiRootUri)
+
+  @Bean
+  fun probationOffenderSearchWebClient(authorizedClientManager: OAuth2AuthorizedClientManager, builder: WebClient.Builder): WebClient =
+    authorisedWebClient(authorizedClientManager, builder, registrationId = "probation-offender-search", url = probationOffenderSearchUri)
+
+  private fun authorisedWebClient(
+    authorizedClientManager: OAuth2AuthorizedClientManager,
+    builder: WebClient.Builder,
+    registrationId: String,
+    url: String,
+  ): WebClient {
+    val oauth2Client = ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager).kotlinApply {
+      setDefaultClientRegistrationId(registrationId)
+    }
+
+    return builder.baseUrl(url)
+      .clientConnector(ReactorClientHttpConnector(HttpClient.create().responseTimeout(timeout)))
+      .filter(oauth2Client)
       .build()
-  }
-
-  @Bean
-  fun prisonApiWebClient(authorizedClientManager: OAuth2AuthorizedClientManager): WebClient {
-    val oauth2Client = ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager)
-    oauth2Client.setDefaultClientRegistrationId("prison-api")
-
-    return WebClient.builder()
-      .baseUrl(prisonapiRootUri)
-      .apply(oauth2Client.oauth2Configuration())
-      .build()
-  }
-
-  @Bean
-  fun probationOffenderSearchWebClient(authorizedClientManager: OAuth2AuthorizedClientManager): WebClient {
-    val oauth2Client = ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager)
-    oauth2Client.setDefaultClientRegistrationId("probation-offender-search")
-
-    return WebClient.builder()
-      .baseUrl(probationOffenderSearchUri)
-      .apply(oauth2Client.oauth2Configuration())
-      .build()
-  }
-
-  @Bean
-  fun communityApiHealthWebClient(): WebClient {
-    return WebClient.builder().baseUrl(communityRootUri).build()
-  }
-
-  @Bean
-  fun prisonApiHealthWebClient(): WebClient {
-    return WebClient.builder().baseUrl(prisonapiRootUri).build()
-  }
-
-  @Bean
-  fun oauthApiHealthWebClient(): WebClient {
-    return WebClient.builder().baseUrl(oauthRootUri).build()
   }
 
   @Bean
   fun authorizedClientManager(
-    clientRegistrationRepository: ClientRegistrationRepository?,
-    oAuth2AuthorizedClientService: OAuth2AuthorizedClientService?,
-  ): OAuth2AuthorizedClientManager? {
+    clientRegistrationRepository: ClientRegistrationRepository,
+    oAuth2AuthorizedClientService: OAuth2AuthorizedClientService,
+  ): OAuth2AuthorizedClientManager {
     val authorizedClientProvider = OAuth2AuthorizedClientProviderBuilder.builder().clientCredentials().build()
-    val authorizedClientManager = AuthorizedClientServiceOAuth2AuthorizedClientManager(clientRegistrationRepository, oAuth2AuthorizedClientService)
-    authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider)
-    return authorizedClientManager
+    return AuthorizedClientServiceOAuth2AuthorizedClientManager(
+      clientRegistrationRepository,
+      oAuth2AuthorizedClientService,
+    ).kotlinApply { setAuthorizedClientProvider(authorizedClientProvider) }
   }
 }
