6eec40ded25b60a9473e2c2947f91b8a998069e1
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2024-02-23 12:08:17 +0000

    SDIT-1527: 🔒️ Upgrade spring boot to 3.2.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.2"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.3"
   kotlin("plugin.spring") version "1.9.22"
 }
 
@@ -18,7 +18,7 @@ dependencies {
   testImplementation("io.jsonwebtoken:jjwt-impl:0.12.5")
   testImplementation("io.jsonwebtoken:jjwt-jackson:0.12.5")
   testImplementation("io.projectreactor:reactor-test:3.6.2")
-  testImplementation("org.wiremock:wiremock-standalone:3.3.1")
+  testImplementation("org.wiremock:wiremock-standalone:3.4.1")
   testImplementation("io.opentelemetry:opentelemetry-sdk-testing")
   testImplementation("com.google.code.gson:gson:2.10.1")
   testImplementation("io.swagger.parser.v3:swagger-parser:2.1.20") {
