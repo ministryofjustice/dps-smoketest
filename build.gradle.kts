a2cfb1d218a3cc9ac03969eed2dbc098020e2649
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2024-03-19 08:56:16 +0000

    SDIT-1600: 🔧 Try out latest 3.0 helm chart

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,19 +9,19 @@ configurations {
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:0.2.1")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:0.2.2")
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:3.1.1")
 
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
-  implementation("org.awaitility:awaitility-kotlin:4.2.0")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.4.0")
+  implementation("org.awaitility:awaitility-kotlin:4.2.1")
 
   testImplementation("io.jsonwebtoken:jjwt-impl:0.12.5")
   testImplementation("io.jsonwebtoken:jjwt-jackson:0.12.5")
-  testImplementation("io.projectreactor:reactor-test:3.6.2")
-  testImplementation("org.wiremock:wiremock-standalone:3.4.1")
+  testImplementation("io.projectreactor:reactor-test:3.6.4")
+  testImplementation("org.wiremock:wiremock-standalone:3.4.2")
   testImplementation("io.opentelemetry:opentelemetry-sdk-testing")
   testImplementation("com.google.code.gson:gson:2.10.1")
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.20") {
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.21") {
     exclude(group = "io.swagger.core.v3")
   }
   testImplementation("io.swagger.core.v3:swagger-core-jakarta:2.2.20")
