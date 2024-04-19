bdf4d45c00bc069bc3aa13bc09c8fac88c619d3c
petergphillips <5099053+petergphillips@users.noreply.github.com>
2024-04-19 11:09:00 +0100

    SDIT-1677: ⬆️ Upgrade to latest spring boot (#182)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.5"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.6"
   kotlin("plugin.spring") version "1.9.23"
 }
 
@@ -10,18 +10,18 @@ configurations {
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:0.2.2")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:3.1.1")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:3.1.2")
 
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.4.0")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
   implementation("org.awaitility:awaitility-kotlin:4.2.1")
 
   testImplementation("io.jsonwebtoken:jjwt-impl:0.12.5")
   testImplementation("io.jsonwebtoken:jjwt-jackson:0.12.5")
-  testImplementation("io.projectreactor:reactor-test:3.6.4")
-  testImplementation("org.wiremock:wiremock-standalone:3.4.2")
+  testImplementation("io.projectreactor:reactor-test:3.6.5")
+  testImplementation("org.wiremock:wiremock-standalone:3.5.3")
   testImplementation("io.opentelemetry:opentelemetry-sdk-testing")
   testImplementation("com.google.code.gson:gson:2.10.1")
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.21") {
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.22") {
     exclude(group = "io.swagger.core.v3")
   }
   testImplementation("io.swagger.core.v3:swagger-core-jakarta:2.2.21")
