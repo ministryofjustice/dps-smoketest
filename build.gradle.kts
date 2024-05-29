c84e19f303e4ab2a3c5419aea1796057adc14138
petergphillips <5099053+petergphillips@users.noreply.github.com>
2024-05-29 13:14:40 +0100

    SDIT-1788: ⬆️ Upgrade to latest spring boot etc. (#188)
    * SDIT-1788: ⬆️ Upgrade to latest spring boot etc.
    
    * SDIT-1788: ⬆️ Upgrade to latest kotlin lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.6"
-  kotlin("plugin.spring") version "1.9.23"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.0"
+  kotlin("plugin.spring") version "2.0.0"
 }
 
 configurations {
@@ -9,22 +9,22 @@ configurations {
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:0.2.4")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:3.1.3")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.0.0")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:4.0.0")
 
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
   implementation("org.awaitility:awaitility-kotlin:4.2.1")
 
   testImplementation("io.jsonwebtoken:jjwt-impl:0.12.5")
   testImplementation("io.jsonwebtoken:jjwt-jackson:0.12.5")
-  testImplementation("io.projectreactor:reactor-test:3.6.5")
-  testImplementation("org.wiremock:wiremock-standalone:3.5.4")
+  testImplementation("io.projectreactor:reactor-test:3.6.6")
+  testImplementation("org.wiremock:wiremock-standalone:3.6.0")
   testImplementation("io.opentelemetry:opentelemetry-sdk-testing")
-  testImplementation("com.google.code.gson:gson:2.10.1")
+  testImplementation("com.google.code.gson:gson:2.11.0")
   testImplementation("io.swagger.parser.v3:swagger-parser:2.1.22") {
     exclude(group = "io.swagger.core.v3")
   }
-  testImplementation("io.swagger.core.v3:swagger-core-jakarta:2.2.21")
+  testImplementation("io.swagger.core.v3:swagger-core-jakarta:2.2.22")
 }
 
 kotlin {
@@ -33,8 +33,6 @@ kotlin {
 
 tasks {
   withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions {
-      jvmTarget = "21"
-    }
+    compilerOptions.jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
   }
 }
