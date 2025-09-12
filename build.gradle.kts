12604d9b365a62dfc9d676259ee9977e2c72ca85
petergphillips <5099053+petergphillips@users.noreply.github.com>
2025-09-12 09:53:14 +0100

    SDIT-3027: ⬆️  Upgrade kotlin lib starter for false positive (#264)

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,17 +9,17 @@ configurations {
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.6.0-beta4")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.7.0-beta")
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.4.10")
 
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.13")
   implementation("org.awaitility:awaitility-kotlin:4.3.0")
 
-  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.6.0-beta4")
-  testImplementation("io.projectreactor:reactor-test:3.7.9")
+  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.7.0-beta")
+  testImplementation("io.projectreactor:reactor-test:3.7.11")
   testImplementation("org.wiremock:wiremock-standalone:3.13.1")
   testImplementation("io.opentelemetry:opentelemetry-sdk-testing")
-  testImplementation("com.google.code.gson:gson:2.13.1")
+  testImplementation("com.google.code.gson:gson:2.13.2")
   testImplementation("io.swagger.parser.v3:swagger-parser:2.1.33") {
     exclude(group = "io.swagger.core.v3")
   }
