b8391774af5531015db96cd2df97639738da3f07
Mike Halma <mike.halma@digital.justice.gov.uk>
2025-09-30 23:57:57 +0100

    SDIT-3046 Try out latests SQS lib with no config change for regression testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ configurations {
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.7.0")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:5.5.0-beta")
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.4.11")
 
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.13")
